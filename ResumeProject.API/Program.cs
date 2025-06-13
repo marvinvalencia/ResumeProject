ab80fa49513fe3366435171e9e067452d5b50714
marvinvalencia <jmarvinvalencia@gmail.com>
2025-06-13 14:01:06 +0800

    comment out basic auth

--- ResumeProject.API/Program.cs
+++ ResumeProject.API/Program.cs
@@ -20,34 +20,34 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.Use(async (context, next) =>
-{
-    if (context.Request.Path.StartsWithSegments("/swagger"))
-    {
-        string? authHeader = context.Request.Headers.Authorization;
+////app.Use(async (context, next) =>
+////{
+////    if (context.Request.Path.StartsWithSegments("/swagger"))
+////    {
+////        string? authHeader = context.Request.Headers.Authorization;
 
-        if (authHeader != null && authHeader.StartsWith("Basic "))
-        {
-            var encodedUsernamePassword = authHeader.Substring("Basic ".Length).Trim();
-            var decodedUsernamePassword = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(encodedUsernamePassword));
-            var username = decodedUsernamePassword.Split(':')[0];
-            var password = decodedUsernamePassword.Split(':')[1];
+////        if (authHeader != null && authHeader.StartsWith("Basic "))
+////        {
+////            var encodedUsernamePassword = authHeader.Substring("Basic ".Length).Trim();
+////            var decodedUsernamePassword = System.Text.Encoding.UTF8.GetString(Convert.FromBase64String(encodedUsernamePassword));
+////            var username = decodedUsernamePassword.Split(':')[0];
+////            var password = decodedUsernamePassword.Split(':')[1];
 
-            if (username == "yourusername" && password == "yourpassword")
-            {
-                await next.Invoke();
-                return;
-            }
-        }
+////            if (username == "yourusername" && password == "yourpassword")
+////            {
+////                await next.Invoke();
+////                return;
+////            }
+////        }
 
-        context.Response.Headers["WWW-Authenticate"] = "Basic";
-        context.Response.StatusCode = 401;
-        await context.Response.WriteAsync("Unauthorized");
-        return;
-    }
+////        context.Response.Headers["WWW-Authenticate"] = "Basic";
+////        context.Response.StatusCode = 401;
+////        await context.Response.WriteAsync("Unauthorized");
+////        return;
+////    }
 
-    await next.Invoke();
-});
+////    await next.Invoke();
+////});
 
 app.UseHttpsRedirection();
 
