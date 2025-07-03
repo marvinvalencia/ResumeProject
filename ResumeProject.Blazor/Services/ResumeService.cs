d359c50a363f817dc82cc501b7b3e769873a41a7
marvinvalencia <jmarvinvalencia@gmail.com>
2025-07-03 21:07:20 +0800

    Added fallback page in case of out of credits in my rest api

--- ResumeProject.Blazor/Services/ResumeService.cs
+++ ResumeProject.Blazor/Services/ResumeService.cs
@@ -23,13 +23,31 @@ namespace ResumeProject.Blazor.Services
         }
 
         /// <summary>
+        /// Gets the Resume.
+        /// </summary>
+        public Resume? Resume { get; private set; } = null;
+
+        /// <summary>
+        /// Gets the Error Message.
+        /// </summary>
+        public string? ErrorMessage { get; private set; } = string.Empty;
+
+        /// <summary>
         /// The GetResumesAsync method retrieves resume by its Id.
         /// </summary>
         /// <param name="resumeId">The resume Id.</param>
         /// <returns>The resume.</returns>
         public async Task<Resume?> GetResumeAsync(Guid resumeId)
         {
-            return await this.http.GetFromJsonAsync<Resume>($"api/resume/{resumeId}");
+            try
+            {
+                this.Resume = await this.http.GetFromJsonAsync<Resume>($"api/resume/{resumeId}");
+            }
+            catch
+            {
+                this.ErrorMessage = "⚠️ This is a static fallback because the API might be down or out of credits.";
+            }
+            return this.Resume;
         }
 
         /// <summary>
